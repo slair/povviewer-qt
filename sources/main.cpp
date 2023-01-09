47322059ea1d43c43dc8ceb1e867702e66423e69
slair <slair@users.noreply.github.com>
2023-01-09 18:38:17 +0400

    autocommit

--- sources/main.cpp
+++ sources/main.cpp
@@ -151,6 +151,7 @@ int main(int argc, char *argv[])
 
 	// todo: 2. chdir to scene file if workdir!=scenedir
 	if (QDir::currentPath() != fi_scene.absolutePath()) {
+
 	}
 
 	qDebug() << "Working dir" << QDir::currentPath();
